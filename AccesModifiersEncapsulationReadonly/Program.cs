ae077fb6b3e121f5c19ecbaa708c413220167a75
Orkhanvg <100156013+Orkhanvg@users.noreply.github.com>
2022-03-18 17:51:29 +0400

    Add files via upload

---
+++ AccesModifiersEncapsulationReadonly/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using AccesModifiersEncapsulationReadonly.userclass;
+
+namespace AccesModifiersEncapsulationReadonly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Task: 
+             * User class
+
+             - Username
+             - Age
+             - Password
+
+             Username, password property-ləri olmadan user obyekti yaratmaq olmaz.
+
+             Age propery-i mənfi ola bilməz.
+
+             Password property-i bu şərtləri ödəməlidir:
+
+             1. Boş ola bilməz
+             2. Uzunluğu 8-dən kiçik ola bilməz
+             3. İçində ən az bir böyük hərf olmalıdır
+             4. İçində ən az bir rəqəm olmalıdır
+
+             Üçüncü və dördüncü şərti ayrı bir private methodda yazıb gəlib propertyinin setində istifadə etmək lazımdır.
+             */
+
+            Console.Write("Please enter the username:             ");
+            string username = Console.ReadLine();
+
+            Console.Write("Please enter the password:             ");
+            string password = Console.ReadLine();
+
+            Console.Write("Please enter the age:             ");
+            string age = Console.ReadLine();
+
+
+            user user1 = new user(username, password);
+
+            user.Info();
+            
+
+
+        }
+
+    }
+}
+
+    
---
+++ AccesModifiersEncapsulationReadonly/user class/user.cs
@@ -0,0 +1,134 @@
+﻿using System;
+namespace AccesModifiersEncapsulationReadonly.userclass
+{
+    public class user
+    {
+        public string Username;
+        private int _age;
+        private string _password;
+
+        public user(string userName, string passWord)
+
+        {
+            Username = userName;
+            Password = passWord;
+
+        }
+        public string username
+        {
+            get
+            {
+                { return username; }
+
+
+            }
+            set
+            {
+                { username = value; }
+
+            }
+
+
+
+        }
+        public int age
+        {
+
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Age is not correct");
+                    return;
+                }
+                    
+
+                _age = value;
+
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+
+
+
+
+            set
+            {
+                if (String.IsNullOrEmpty(value) == false && value.Length >= 8 == true && DifferentCase(value) == true)
+                {
+                    _password = value;
+                    return;
+
+                }
+                Console.WriteLine("Password invalid try again:            ");
+                return;
+
+
+            }
+
+
+
+        }
+
+        internal static void Info()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DifferentCase(string pass)
+        {
+            char[] arr = pass.ToCharArray();
+            bool result = false;
+            bool result1 = false;
+            bool result2 = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+
+                if (result1 != char.IsUpper(arr[i]))
+                {
+                    result = true;
+                }
+
+                if (result2 != char.IsNumber(arr[i]))
+                {
+                    result = true;
+                }
+
+                if (result1 == true && result2 == true)
+                {
+                    result = true;
+                }
+                return result;
+            }
+            return result;
+        }
+
+       
+
+
+    }
+
+    public void Info()
+
+    {
+        Console.WriteLine("User info");
+    }
+
+}
+    
+      
+
+
+
+
+ 
+
